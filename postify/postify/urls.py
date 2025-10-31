178de75d774c5b1a449196418c991d4021a32bb4
imane iramdane <imaniramdan696@gmail.com>
2025-10-31 17:11:57 +0100

    Profile

--- postify/postify/urls.py
+++ postify/postify/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path,include
 from users import views as user_views
 from django.contrib.auth import views as auth_views 
+from django.conf.urls.static import static
+from django.conf import settings 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,3 +15,7 @@ urlpatterns = [
     path('', include('posts.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
